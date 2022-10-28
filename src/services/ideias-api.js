b7fd42d92b3412e17a8f695e6e01067200992a73
daniela.oliveira <daniela.oliveira@ngi.com.br>
2022-10-28 08:26:01 -0300

    sendIdea

--- src/services/ideias-api.js
+++ src/services/ideias-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let apiIdeas = {}
 
 apiIdeas.sendIdea = async (idea) => {
-    let data = await axios.post("http://localhost:3000/persons/", { body: { idea: idea } })
+    let data = await axios.post("http://localhost:3000/idea/", { body: { idea: idea } })
     console.log(data);
     return data;
 }
